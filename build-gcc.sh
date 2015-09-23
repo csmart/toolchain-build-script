1798b765cf5c01b69d1802d49d9fb91075ce128d
Michael Ellerman <mpe@ellerman.id.au>
2015-09-23 13:01:16 +1000

    Add endian support

--- build-gcc.sh
+++ build-gcc.sh
@@ -3,17 +3,27 @@
 # Based on https://github.com/antonblanchard/jenkins-scripts/blob/master/gcc_kernel_build.sh
 
 BRANCH=$1
+ENDIAN=$2
 
-if [[ -z "$BRANCH" ]]; then
-	echo "Usage: $0 <4|5|head>" >&2
+if [[ -z "$BRANCH" || -z "$ENDIAN" ]]; then
+	echo "Usage: $0 <4|5|head> <big|little|both>" >&2
 	exit 1
 fi
-
-TARGETS="--target=powerpc-linux --enable-targets=powerpc-linux,powerpc64-linux,powerpcle-linux,powerpc64le-linux"
 
 set -o errexit
 set -o pipefail
 set -o nounset
+
+if [[ $ENDIAN == "big" ]]; then
+	TARGETS="--target=powerpc64-linux --enable-targets=powerpc-linux,powerpc64-linux"
+	NAME="powerpc64-linux"
+elif [[ $ENDIAN == "little" ]]; then
+	TARGETS="--target=powerpc64le-linux --enable-targets=powerpcle-linux,powerpc64le-linux"
+	NAME="powerpc64le-linux"
+else
+	TARGETS="--target=powerpc-linux --enable-targets=powerpc-linux,powerpc64-linux,powerpcle-linux,powerpc64le-linux"
+	NAME="powerpc-linux"
+fi
 
 BASEDIR=$PWD/gcc-build-$$
 PARALLEL=-j$(nproc)
@@ -49,7 +59,7 @@ git clone $branch --depth=100 -q git://fs.ozlabs.ibm.com/mirror/gcc.git
 (cd gcc; git log -1)
 
 VERSION=$(< gcc/gcc/BASE-VER)
-DESTDIR=/opt/cross/gcc-$VERSION-nolibc/powerpc-linux/
+DESTDIR=/opt/cross/gcc-$VERSION-nolibc/$NAME/
 
 mkdir -p $DESTDIR || {
 	echo "Error: can't write to $DESTDIR" >&2
@@ -72,14 +82,14 @@ make -s install
 echo "Building gcc ..."
 cd $BASEDIR/build/gcc
 
-../../src/gcc/configure --prefix=$BASEDIR/install/powerpc-linux --disable-multilib --disable-bootstrap --enable-languages=c $TARGETS
+../../src/gcc/configure --prefix=$BASEDIR/install/$NAME --disable-multilib --disable-bootstrap --enable-languages=c $TARGETS
 
 # We don't need libgcc for building the kernel, so keep it simple
 make -s all-gcc $PARALLEL
 make -s install-gcc
 
 echo "Installing to /opt/cross ..."
-rsync -a --delete $BASEDIR/install/powerpc-linux/ /opt/cross/gcc-$VERSION-nolibc/powerpc-linux/
+rsync -a --delete $BASEDIR/install/$NAME/ /opt/cross/gcc-$VERSION-nolibc/$NAME/
 
 echo "=================================================="
 echo " OK"
